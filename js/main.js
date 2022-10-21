1f5108c407d987a422ec2151ca80996c94f3d341
Marsha <marshatiisa35@gmail.com>
2022-10-21 15:02:04 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// target the inputs using value
+document.querySelector('button').addEventListener('click',generateName)
+//use if conditionals to compare
+// what should i store in the variables i create?
+function generateName() {
+    
+}
