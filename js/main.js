e30108aea09f1e7377aeacfc9e42df05c9d89a52
Elvin Garcia <elvingarcia19@gmail.com>
2022-10-24 11:54:53 -0400

    fixed js

--- js/main.js
+++ js/main.js
@@ -1,100 +1,100 @@
-class WuTang {
-    constructor(colorAnswer, showAnswer, destinationAnswer, movieAnswer, animalAnswer){
-        this._colorAnswer = colorAnswer;
-        this._showAnswer = showAnswer;
-        this._destinationAnswer = destinationAnswer;
-        this._movieAnswer = movieAnswer;
-        this._animalAnswer = animalAnswer
-    }
+// class WuTang {
+//     constructor(colorAnswer, showAnswer, destinationAnswer, movieAnswer, animalAnswer){
+//         this._colorAnswer = colorAnswer;
+//         this._showAnswer = showAnswer;
+//         this._destinationAnswer = destinationAnswer;
+//         this._movieAnswer = movieAnswer;
+//         this._animalAnswer = animalAnswer
+//     }
 
-    // Getter methods
-    get colorAnswer() {
-        return this._colorAnswer;
-    }
+//     // Getter methods
+//     get colorAnswer() {
+//         return this._colorAnswer;
+//     }
 
-    get showAnswer() {
-        return this._showAnswer;
-    }
+//     get showAnswer() {
+//         return this._showAnswer;
+//     }
 
-    get destinationAnswer() {
-        return this._destinationAnswer;
-    }
+//     get destinationAnswer() {
+//         return this._destinationAnswer;
+//     }
 
-    get movieAnswer() {
-        return this._movieAnswer;
-    }
+//     get movieAnswer() {
+//         return this._movieAnswer;
+//     }
 
-    get animalAnswer() {
-        return this._animalAnswer;
-    }
-
-
-    // Setter method
-    set colorAnswer(value) {
-        this._colorAnswer = value
-    }
-
-    set showAnswer(value) {
-        this._showAnswer = value
-    }
-
-    set destinationAnswer(value) {
-        this._destinationAnswer = value
-    }
-
-    set movieAnswer(value) {
-        this._movieAnswer = value
-    }
-
-    set animalAnswer(value) {
-        this._animalAnswer = value
-    }
-
-    // Method gets value from the DOM
-    valueFromDom(id) {
-        return document.querySelector(id).value
-    }
+//     get animalAnswer() {
+//         return this._animalAnswer;
+//     }
 
 
-    // Method uses fetch to make a request
-    async retrieveData() {
+//     // Setter method
+//     set colorAnswer(value) {
+//         this._colorAnswer = value
+//     }
 
-        try {
-            const response = await fetch(`/api?answers=${this._colorAnswer} ${this._showAnswer} ${this._destinationAnswer} ${this._movieAnswer} ${this._animalAnswer}`)
-            const data = await response.json()
+//     set showAnswer(value) {
+//         this._showAnswer = value
+//     }
 
-            console.log(data)
-            //Place Name in the DOM
-            document.querySelector("#placeName").innerText = `${data.firstName} ${data.secondName}`
+//     set destinationAnswer(value) {
+//         this._destinationAnswer = value
+//     }
 
-        } catch (error) {
-            console.log(error)
-        }
-    }
-}
+//     set movieAnswer(value) {
+//         this._movieAnswer = value
+//     }
 
-document.querySelector('button').addEventListener('click', () => {
+//     set animalAnswer(value) {
+//         this._animalAnswer = value
+//     }
 
-    // Create an instance of the class Palindrome
-    const wutang = new WuTang()
-
-    //get the value from the input
-    let color = wutang.valueFromDom('#colors')
-    let show = wutang.valueFromDom('#show')
-    let destination = wutang.valueFromDom('#destination')
-    let movie = wutang.valueFromDom('#movie')
-    let animal = wutang.valueFromDom('#animal')
+//     // Method gets value from the DOM
+//     valueFromDom(id) {
+//         return document.querySelector(id).value
+//     }
 
 
-    // Set the value in the Class
-    wutang.colorAnswer = color
-    wutang.showAnswer = show
-    wutang.destinationAnswer = destination
-    wutang.movieAnswer = movie
-    wutang.animalAnswer = animal 
+//     // Method uses fetch to make a request
+//     async retrieveData() {
 
-    // Call the fetch
-    wutang.retrieveData()
+//         try {
+//             const response = await fetch(`/api?answers=${this._colorAnswer} ${this._showAnswer} ${this._destinationAnswer} ${this._movieAnswer} ${this._animalAnswer}`)
+//             const data = await response.json()
+
+//             console.log(data)
+//             //Place Name in the DOM
+//             document.querySelector("#placeName").innerText = `${data.firstName} ${data.secondName}`
+
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+// }
+
+// document.querySelector('button').addEventListener('click', () => {
+
+//     // Create an instance of the class Palindrome
+//     const wutang = new WuTang()
+
+//     //get the value from the input
+//     let color = wutang.valueFromDom('#colors')
+//     let show = wutang.valueFromDom('#show')
+//     let destination = wutang.valueFromDom('#destination')
+//     let movie = wutang.valueFromDom('#movie')
+//     let animal = wutang.valueFromDom('#animal')
+
+
+//     // Set the value in the Class
+//     wutang.colorAnswer = color
+//     wutang.showAnswer = show
+//     wutang.destinationAnswer = destination
+//     wutang.movieAnswer = movie
+//     wutang.animalAnswer = animal 
+
+//     // Call the fetch
+//     wutang.retrieveData()
     
 
-})
+// })
