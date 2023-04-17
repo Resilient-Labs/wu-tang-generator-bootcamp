4d8f8af9c663e019f7acb3ff93feb937487a8f2b
Jada-Cherie <jada.c.townsend@gmail.com>
2023-04-17 15:43:55 -0400

    update to JS: it works

--- JS/app.js
+++ JS/app.js
@@ -48,14 +48,14 @@ function getWuTangName(){
         "Wicked",
         "X-cessive",
         "X-pert",
-        "Zexy",
+        "Zexy"
     ]
 
     //To figure out the first name you have to calculate the index of the first two questions that the user choose and then get the remainder with an array length
-    const queryOne= document.querySelector("#member").value
+    const queryOne = document.querySelector("#member").value
     const queryTwo = document.querySelector("#color").value
     //this change was madew when I tried to run the error through chatgpt
-    const firstName = (parseInt(queryOne) + parseInt(queryTwo)) % adjectives.length
+    const firstName = (queryOne.length + queryTwo.length) % adjectives.length
     console.log(firstName)
     //list of possible last names of Wu Tang Name, I got this list from a repository on github
     const nouns = [
@@ -98,14 +98,14 @@ function getWuTangName(){
         "Warrior",
         "Watcher",
         "Wizard",
-        "Worlock",
+        "Worlock"
     ]
 
     //To figure out the last name you have to calculate the index of the last three questions that the user choose and then get the remainder with an array length
     const queryThree= document.querySelector("#animal").value
     const queryFour = document.querySelector("#weapon").value
     const queryFive = document.querySelector("#weather").value
-    const lastName = (queryThree + queryFour + queryFive) % nouns.length
+    const lastName = (queryThree.length + queryFour.length + queryFive.length) % nouns.length
     // console.log(lastName)
 
 
